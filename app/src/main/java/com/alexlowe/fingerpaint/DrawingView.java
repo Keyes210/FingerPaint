042fb986dfcc26f6c377d2819c7d3e3626aff373
Alex Lowe <loweab@gmail.com>
2015-08-22 14:43:56 -0400

    user can draw on screen

--- app/src/main/java/com/alexlowe/fingerpaint/DrawingView.java
+++ app/src/main/java/com/alexlowe/fingerpaint/DrawingView.java
@@ -4,8 +4,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Alex on 8/22/2015.
@@ -15,13 +21,15 @@ public class DrawingView extends View {
     private final int paintColor = Color.BLACK;
     // defines paint and canvas
     private Paint drawPaint;
+    // Store circles to draw each time the user touches down
+    private List<Point> circlePoints;
+
     
 
     public DrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setFocusable(true);
-        setFocusableInTouchMode(true);
         setupPaint();
+        circlePoints = new ArrayList<Point>();
     }
 
     private void setupPaint() {
@@ -29,17 +37,29 @@ public class DrawingView extends View {
         drawPaint.setColor(paintColor);
         drawPaint.setAntiAlias(true);
         drawPaint.setStrokeWidth(5);
-        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStyle(Paint.Style.FILL);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
     }
 
+    // Draw each circle onto the view
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(50, 50, 20, drawPaint);
-        drawPaint.setColor(Color.GREEN);
-        canvas.drawCircle(50, 150, 20, drawPaint);
-        drawPaint.setColor(Color.CYAN);
-        canvas.drawCircle(50, 250, 20, drawPaint);
+        for(Point p : circlePoints){
+            canvas.drawCircle(p.x, p.y, 5, drawPaint);
+        }
+    }
+
+    // Append new circle each time user presses on screen
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        circlePoints.add(new Point(Math.round(touchX), Math.round(touchY)));
+
+        // indicate view should be redrawn
+        postInvalidate();
+        return true;
     }
 }
